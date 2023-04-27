1130883a354a85c37d384017817cf72df559c81a
alli-cpp <alli-cpp@github.com>
2023-04-27 22:42:48 +0000

    room

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,14 +1,5 @@
-// Only make Promise
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    /* eslint-disable */
-      if (true) {
-        resolve();
-      } else {
-        reject();
-      }
-      /* eslint-enable */
-  });
+export default class ClassRoom {
+  constructor(maxStudentSize) {
+    this._maxStudentsSize = maxStudentSize;
+  }
 }
-
-export default getResponseFromAPI;
